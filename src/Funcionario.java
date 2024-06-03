b852d92220b590d79b4c1d6e293b6a34d4f93913
BrunoHunoff <brunohunoff@gmail.com>
2024-06-03 20:02:25 -0300

    Funcionario

---
+++ src/Funcionario.java
@@ -0,0 +1,10 @@
+public class Funcionario {
+
+    public String nome;
+
+    public Funcionario(){};
+
+    public Funcionario(String nome) {
+        this.nome = nome;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Funcionario f1 = new Funcionario("Bruno");
+        System.out.println(f1.nome);
     }
 }
